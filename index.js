62b9982f6717106bee004134443f45a0a2ae53d9
Mary Rankin <mary-rankin@outlook.com>
2021-05-16 17:32:22 -0400

    added form for creating new restaurant, saving to db

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+// PACKAGES 
 const express = require('express')
+const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
-const app = express()
 
-const mongoDbUrl = "mongodb://localhost:27017/restaurant-game";
+
+
+// DATABASE CONNECTION
+const mongoDbUrl = 'mongodb://localhost:27017/restaurant-game'
 
 mongoose.connect(mongoDbUrl, {
     useNewUrlParser: true,
@@ -16,12 +20,26 @@ mongoose.connection.once("open", () => {
     console.log("database connected");
 })
 
+
+
+// MONGOOSE MODELS
 const Restaurant = require('./models/restaurant')
 
+
+
+// VIEW ENGINE/EJS
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+
+
+// POST REQUEST STUFF
+app.use(express.urlencoded({ extended: true }))
+
+
+
+// ROUTES
 app.get('/', (req, res) => {
     res.render('home')
 })
@@ -30,6 +48,22 @@ app.get('/restaurants', (req, res) => {
     res.render('restaurants/index')
 })
 
+app.get('/restaurants/new', (req, res) => {
+    res.render('restaurants/new')
+})
+
+app.post('/restaurants', async (req, res) => {
+    const restaurant = new Restaurant(req.body.restaurant)
+    restaurant.numEmployees = 0
+    restaurant.profit = 0
+    restaurant.rating = 1
+    await restaurant.save()
+    res.redirect('/restaurants')
+})
+
+
+
+// EXPRESS PORT
 app.listen(3000, () => {
     console.log("serving on port 3000")
 })
--- models/restaurant.js
+++ models/restaurant.js
@@ -3,13 +3,34 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const RestaurantSchema = new Schema({
-    name: String,
-    type: String,
-    location: String,
-    menu: [String],
-    numEmployees: Number,
-    profit: Number,
-    rating: Number,
+    name: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    menu: {
+        type: [String],
+        required: true
+    },
+    numEmployees: {
+        type: Number,
+        required: true
+    },
+    profit: {
+        type: Number,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    }
 })
 
 //in future, add owner
