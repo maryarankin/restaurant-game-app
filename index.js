e044f382a4bd638e838bb9c91228ed53af28caa0
Mary Rankin <mary-rankin@outlook.com>
2021-05-16 15:58:42 -0400

    connected mongo database

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
 const app = express()
+
+const mongoDbUrl = "mongodb://localhost:27017/restaurant-game";
+
+mongoose.connect(mongoDbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on("error", console.error.bind(console, "connection error:"));
+mongoose.connection.once("open", () => {
+    console.log("database connected");
+})
 
 app.get('/', (req, res) => {
     res.send('hi')
