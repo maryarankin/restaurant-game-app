ef38ea9401a45c3adbc76580422e9bbc0ba35233
Mary Rankin <mary-rankin@outlook.com>
2021-07-24 17:12:10 -0400

    testing randomEvents fxn

--- randomEvents.js
+++ randomEvents.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose')
 const User = require('./models/user')
 
 module.exports.randomEvents = async (user, req) => {
+    console.log('test')
     let message = ''
     let messageType = ''
     if (user.month == 1 && user.day == 14) {
         message = 'you found 5 dollars on the ground'
-        messageType ='success'
+        messageType = 'success'
         user.money += 5
         await user.save()
         req.flash(messageType, message)
+        console.log('test2')
     }
 }
